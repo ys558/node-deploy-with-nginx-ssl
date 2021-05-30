ebedf4c277b86ff2555f9f3fa564cd2eddfcdfc0
yuyi <38847066+ys558@users.noreply.github.com>
2021-05-30 15:20:52 +0800

    simple node for deploy

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res) => res.send('<h1>Node App</h1>'))
+app.listen(5000, () => console.log('App listening on port 5000'))
